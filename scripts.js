8ece8ca39f67bc804a55a79c2512d80199064d73
Noah Kittleson <noahkittleson@gmail.com>
2017-02-27 12:04:34 -0800

    the javascript actually works now

--- scripts.js
+++ scripts.js
@@ -2,15 +2,19 @@ $(document).ready(function() {
 
   var quotes = ['quote1', 'quote2'];
   var iii = 0;
-  for (var i = 1; i < array.length; i++) {
-    $("#" + array[i]).hide();
+  for (var i = 1; i < quotes.length; i++) {
+    $("#" + quotes[i]).hide();
   }
+  var fadeLength = 400;
+
   setInterval(function() {
-    $("#" + quotes[iii]).hide();
+    $("#" + quotes[iii]).fadeOut(fadeLength);
     iii++;
     if (iii >= quotes.length) {
       iii = 0;
     }
-    $("#" + quotes[iii]).show();
-  }, 3000);
-}
+    setTimeout(function(fadeLength ) {
+      $("#" + quotes[iii]).fadeIn();
+    }, fadeLength);
+  }, 5000);
+})
